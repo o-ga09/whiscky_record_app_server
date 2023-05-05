1c824aef1b98a028709266cb44b3849ba5eb9ac2
taiti09 <taitiabe1997@gmail.com>
2023-05-05 20:06:14 +0900

    #2 #3 fix:テーブル定義追加

--- _tools/mysql/schema.sql
+++ _tools/mysql/schema.sql
@@ -6,3 +6,11 @@ CREATE TABLE `user`
     `modified_at` DATETIME(6) NOT NULL COMMENT 'ユーザーログイン日時',
     PRIMARY KEY (`id`)
 ) Engine=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='ユーザー';
+
+CREATE TABLE whisky_record (
+  user_id VARCHAR(255) NOT NULL,
+  whisky_name VARCHAR(255) NOT NULL,
+  drankAt DATE NOT NULL,
+  imageUrl VARCHAR(255),
+  FOREIGN KEY (user_id) REFERENCES user(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
