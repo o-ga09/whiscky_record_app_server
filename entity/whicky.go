d4484c1aa000c825faafa612d77b7078d1c9bd7f
o-ga <54522966+o-ga09@users.noreply.github.com>
2023-05-10 09:48:03 +0900

    fix: DBに対応する構造体およびSQL修正

--- entity/whicky.go
+++ entity/whicky.go
@@ -8,5 +8,6 @@ type WhickyRecord struct {
     UserID   string      `db:"user_id" json:"userId"`
     Name     string     `db:"whisky_name" json:"name"`
     DrankAt  time.Time `db:"drankAt" json:"drankAt"`
+    evaluate string `db:"evaluate" json:"evaluate"`
     ImageURL string     `db:"imageUrl" json:"imageUrl"`
 }
