e12d779c253afcd2bfd87bb5f09e9c97f82e2569
taiti09 <taitiabe1997@gmail.com>
2023-05-13 20:06:29 +0900

    fix: DBスキーマ変更 記録用テーブルに情報追加

--- _tools/mysql/schema.sql
+++ _tools/mysql/schema.sql
@@ -1,13 +1,13 @@
 CREATE TABLE `user`
 (
-    `user_id` VARCHAR(32) NOT NULL COMMENT 'ユーザー名のuuid',
+    `user_id` VARCHAR(80) NOT NULL COMMENT 'ユーザー名のid',
     `created_at` DATETIME(6) NOT NULL COMMENT 'ユーザー登録日時',
     `modified_at` DATETIME(6) NOT NULL COMMENT 'ユーザーログイン日時',
     PRIMARY KEY (`user_id`)
 ) Engine=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='ユーザー';
 
 CREATE TABLE `whicky_record` (
-  `user_id` VARCHAR(32) NOT NULL,
+  `user_id` VARCHAR(80) NOT NULL,
   `whisky_name` VARCHAR(255) NOT NULL,
   `drankAt` DATE NOT NULL,
   `taste` VARCHAR(255),
